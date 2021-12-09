9303df3b3032e1b746160fd65c672c37197027fc
Yaroslav Oleynik <yaraoley2005@mail.ru>
2021-12-09 03:30:29 +0300

    Starting adding a part with links to my social media accounts

--- resume/src/components/LinkComponent.vue
+++ resume/src/components/LinkComponent.vue
@@ -16,7 +16,16 @@ export default {
 
 <style>
     .link-covering-image{
-        
+        transform: translate(-50%,-50%);
+        background-color: rgba(0, 0, 0, 0.89);
+        border-radius:3px;
+        padding:70px 100px;
+        box-shadow: 0 1px 2px rgba(184, 71, 36, 0.25);
+        transition: box-shadow 0.3s ease-in-out;
+    }
+
+    .link-covering-image:hover{
+        box-shadow: 0 5px 15px rgba(192, 108, 87, 0.6);
     }
 
 </style>
